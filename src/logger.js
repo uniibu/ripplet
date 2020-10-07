0b6894f47e1e72bb4addca295e3b741251acd78f
unibtc <unibtc@gmail.com>
2020-10-07 21:24:34 +0000

    : update logging

--- src/logger.js
+++ src/logger.js
@@ -1,5 +1,7 @@
 const { createLogger, format, transports } = require('winston');
 require('winston-daily-rotate-file');
+const lg = require("@devnodes/logger-client");
+const log = lg.instance("wss://log.nodes.dev", "XRP")
 const { combine, timestamp, printf } = format;
 const logFormat = printf(info => {
   return `${info.timestamp} [${info.level.toUpperCase()}]: ${info.message}`;
@@ -23,11 +25,13 @@ const logger = createLogger({
   transports: [rtransport, ctransport],
   exitOnError: false
 });
+
 const wrap = {};
 const workerId = process.env.NODE_APP_INSTANCE == undefined ? 0 : process.env.NODE_APP_INSTANCE;
 wrap.info = (...args) => {
   args = args.map(a => typeof a !== 'string' ? JSON.stringify(a) : a);
   logger.info(`[WORKER ${workerId}]` + args.join(' '));
+  log.info(`[WORKER ${workerId}]` + args.join(' '));
 };
 wrap.boxen = (args) => {
   logger.info(args);
@@ -35,9 +39,12 @@ wrap.boxen = (args) => {
 wrap.error = (...args) => {
   args = args.map(a => typeof a !== 'string' ? JSON.stringify(a) : a);
   logger.error(`[WORKER ${workerId}]` + args.join(' '));
+  log.error(`[WORKER ${workerId}]` + args.join(' '));
 };
 wrap.warn = (...args) => {
   args = args.map(a => typeof a !== 'string' ? JSON.stringify(a) : a);
   logger.warn(`[WORKER ${workerId}]` + args.join(' '));
+  log.info(`[WORKER ${workerId}]` + args.join(' '));
 };
+wrap.close = log.close;
 module.exports = wrap;
